fa0a08f81d5cb7f92d36d1980e75e7da4ab5599e
usuari <ricardo.garciab5@gmail.com>
2023-03-07 14:07:27 +0100

    algo

---
+++ resources/views/peliculas/create.blade.php
@@ -0,0 +1,28 @@
+Formulario de creacion de peliculas
+
+<form action="" method="post" enctype="multipart/form-data">
+
+
+<label for="Nombre">Nombre</label>
+    <input type="text" name="Nombre" id="Nombre">
+    <br>
+
+<label for="Prouctora">Prouctora</label>
+    <input type="text" name="Prouctora" id="Prouctora">
+    <br>
+
+ <label for="Genero">Genero</label>
+    <input type="text" name="Genero" include_once="Genero">
+    <br>
+
+<label for="Descripción">Descripción</label>
+    <input type="text" name="Descripción" id="Descripción">
+    <br>
+
+    <input type="submit" name="Enviar" id="Enviar">
+    <br>
+
+
+
+
+</form>
---
+++ resources/views/peliculas/fotm.blade.php
@@ -0,0 +1,1 @@
+Formularios que tendra los datos en comun con create y edit
